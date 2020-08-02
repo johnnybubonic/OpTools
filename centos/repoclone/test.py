473833a58f996c09d23d0b3dfac93ded887e7162
brent s <bts@square-r00t.net>
2020-08-02 03:48:24 -0400

    removed repoclone scripts (they've been reformed into the RepoMirror repository) and added libvirt redirection

--- centos/repoclone/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-selfpath = os.path.abspath(os.path.expanduser(__file__))
-print(selfpath)
-
-logmodpath = os.path.abspath(os.path.join(selfpath, '..', '..', '..', 'lib', 'python'))
-print(logmodpath)
